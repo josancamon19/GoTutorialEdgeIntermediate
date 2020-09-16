c8caf5c7fa9aaf7c016e4d970445b6be875f0f28
Joan Cabezas <joan.santiago.cabezas@gmail.com>
2020-09-15 19:45:18 -0500

    Go decorator-functions.go with endpoint auth example

---
+++ decorator-functions.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func simpleFunc() {
+	fmt.Println("Simple func call")
+}
+
+func withFuncArg(funcArg func()) {
+	fmt.Println("withFuncArg")
+	funcArg()
+}
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Checking to see if Authorized header set...")
+
+		if val, ok := r.Header["Authorized"]; ok {
+			fmt.Println(val)
+			if val[0] == "true" {
+				fmt.Println("Header is set! We can serve content!")
+				endpoint(w, r)
+			}
+		} else {
+			fmt.Println("Not Authorized!!")
+			fmt.Fprintf(w, "Not Authorized!!")
+		}
+	})
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: homePage")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+}
+
+func main() {
+	withFuncArg(simpleFunc)
+	http.Handle("/", isAuthorized(homePage))
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
